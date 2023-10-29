19bfa05dd7873b070403dbe67760420beb107dc2
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-29 13:30:06 +0100

    Added handler for NoSuchPersistedEntityExceptions, returning a 400 - Bad Request

--- src/main/java/com/fredrikpedersen/experis_vin_lotteri/web/RestControllerExceptionHandler.java
+++ src/main/java/com/fredrikpedersen/experis_vin_lotteri/web/RestControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.fredrikpedersen.experis_vin_lotteri.web;
 
+import com.fredrikpedersen.experis_vin_lotteri.exceptions.NoSuchPersistedEntityException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 @ControllerAdvice
 public class RestControllerExceptionHandler {
+
+    @ExceptionHandler({NoSuchPersistedEntityException.class})
+    public ResponseEntity<?> handleNoSuchPersistedEntityException(final NoSuchPersistedEntityException exception) {
+        log.error("Tried retrieving non-existing entity: ", exception);
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<?> handleException(final Exception exception) {
